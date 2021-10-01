53dd25ae9c539dc102573374b757e89de4800eed
Carl-Henrik Åkeson <carlhenrik.akeson@gmail.com>
2021-10-01 15:04:09 +0200

    Added tkinter gui

--- src/auto.py
+++ src/auto.py
@@ -1,10 +1,11 @@
-import random
+import random, threading
 from time import sleep
+from PySimpleGUI.PySimpleGUI import Button
 import pyautogui
 import pygetwindow as gw
-import PySimpleGUI as sg
+from tkinter import * # pylint: disable=unused-wildcard-import 
 
-
+start_stop=1
 
 def press_random_wasd():
     #Generate which button to press
@@ -15,19 +16,45 @@ def press_random_wasd():
     return
 
 
-
-
-## Main loop of the program
-def main():
-    x=1
-    while (x==1):
+## On state function
+def main_anti_afk():
+    while (start_stop==1):
+        start_btn["state"] = "disabled"
+        start_btn.config(cursor="watch") 
         if (gw.getActiveWindow().title == "New World"): #Check if new world is open else wait 20s and check again
             press_random_wasd()
         else:
             sleep(20)
+    start_btn["state"] = "normal"
+    start_btn.config(cursor="arrow") #Change back to the normal mouse cursor when it's hovering over
 
-    
+
+def start_up_process():
+    threading.Thread(target=main_anti_afk).start()
+    return
 
 
-if __name__ == "__main__":
-    main()
+def main_stop():
+    global start_stop
+    start_stop=0
+
+def stop_process():
+    threading.Thread(target=main_stop).start()
+
+##GUI Code bellow
+
+#TK set up
+root = Tk()
+root.title(" Auto-AFK-clicker") #Title/Name of program in top bar
+
+
+root.minsize( 150, 100) #minimum window size
+root.geometry("150x100") #Window start up size
+
+start_btn = Button( root, text="Start", command=start_up_process)
+start_btn.grid(row=0, column=0)
+
+stop_btn = Button( root, text="Stop", command=stop_process)
+stop_btn.grid(row=0, column=1)
+
+root.mainloop()
