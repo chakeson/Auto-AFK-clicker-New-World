1dbfa347ed451a62fd27875a3d42a26b70b20d5d
Carl-Henrik Åkeson <carlhenrik.akeson@gmail.com>
2021-10-03 01:01:53 +0200

    Added SendInput support and fixed press

--- src/auto.py
+++ src/auto.py
@@ -2,35 +2,71 @@ import random, threading
 from time import sleep
 from PySimpleGUI.PySimpleGUI import Button
 import pyautogui
+import pydirectinput #Like pyautogui but uses the SendInput https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendinput
 import pygetwindow as gw
 from tkinter import * # pylint: disable=unused-wildcard-import 
 
 start_stop=1
 
-def press_random_wasd():
+def press_random_wasd(args):
     #Generate which button to press
-    val = random.choice(["W","A","S","D"])
-    pyautogui.press(val) #press it
-    time_out_time = random.randint(1,60) #Generate timeout time
-    sleep(time_out_time)
+    
+    
+    if (args == "start"):
+        val = random.choice(["W","A","S","D"])
+        pyautogui.press(val) #press it
+        time_out_time = random.randint(1,60) #Generate timeout time
+        sleep(time_out_time)
+
+    if (args == "long"):
+        val = random.choice(["W","A","S","D"])
+        pyautogui.press(val) #press it
+        time_out_time = random.randint(600,1140) #Generate timeout time
+        sleep(time_out_time)
+    
+    if (args == "start_SendInput"):
+        val = random.choice(["W","A","S","D"])
+        pydirectinput.press(val) #press it
+        time_out_time = random.randint(1,60) #Generate timeout time
+        sleep(time_out_time)
+
+    if (args == "long_SendInput"):
+        val = random.choice(["W","A","S","D"])
+        pydirectinput.press(val) #press it
+        time_out_time = random.randint(600,1140) #Generate timeout time
+        sleep(time_out_time)
+
     return
 
 
 ## On state function
-def main_anti_afk():
+def main_anti_afk(args):
+    start_btn["state"] = "disabled"
+    start_btn.config(cursor="watch")
+    start_btn_long["state"] = "disabled"
+    start_btn_long.config(cursor="watch") 
+
+    start_btn_SendInput["state"] = "disabled"
+    start_btn_SendInput.config(cursor="watch")
+    start_btn_long_SendInput["state"] = "disabled"
+    start_btn_long_SendInput.config(cursor="watch") 
+
     while (start_stop==1):
-        start_btn["state"] = "disabled"
-        start_btn.config(cursor="watch") 
-        #if (gw.getActiveWindow().title == "New World"): #Check if new world is open else wait 20s and check again
-        #    press_random_wasd()
-        #else:
-        sleep(20)
+        press_random_wasd(args)
+
+        
     start_btn["state"] = "normal"
     start_btn.config(cursor="arrow") #Change back to the normal mouse cursor when it's hovering over
+    start_btn_long["state"] = "normal"
+    start_btn_long.config(cursor="arrow")
+    start_btn_SendInput["state"] = "normal"
+    start_btn_SendInput.config(cursor="arrow")
+    start_btn_long_SendInput["state"] = "normal"
+    start_btn_long_SendInput.config(cursor="arrow")
 
 
-def start_up_process():
-    threading.Thread(target=main_anti_afk).start()
+def start_up_process(arguments):
+    threading.Thread(target=main_anti_afk, args=[arguments]).start()
     return
 
 
@@ -48,13 +84,22 @@ root = Tk()
 root.title(" Auto-AFK-clicker") #Title/Name of program in top bar
 
 
-root.minsize( 150, 100) #minimum window size
-root.geometry("150x100") #Window start up size
+root.minsize( 350, 100) #minimum window size
+root.geometry("350x100") #Window start up size
 
-start_btn = Button( root, text="Start", command=start_up_process)
+start_btn = Button( root, text="Start", command=lambda: start_up_process("start"))
 start_btn.grid(row=0, column=0)
 
+start_btn_long = Button( root, text="Start slow timmer", command=lambda: start_up_process("long"))
+start_btn_long.grid(row=0, column=1)
+
+start_btn_SendInput = Button( root, text="Start with SendInput ", command=lambda: start_up_process("start_SendInput"))
+start_btn_SendInput.grid(row=1, column=0)
+
+start_btn_long_SendInput = Button( root, text="Start slow timmer with SendInput", command=lambda: start_up_process("long_SendInput"))
+start_btn_long_SendInput.grid(row=1, column=1)
+
 stop_btn = Button( root, text="Stop", command=stop_process)
-stop_btn.grid(row=0, column=1)
+stop_btn.grid(row=2, column=0)
 
 root.mainloop()
